bb2589ee1dc10184898217b3f9c1194ddc117f90
Nolan Bridges <nolanmbridges@gmail.com>
2024-06-04 19:46:44 -0700

    updates

--- fontmodel.py
+++ fontmodel.py
@@ -13,6 +13,9 @@ class MultiheadSelfAttention(nn.Module):
         self.scale = embedding_dim**(-0.5)
 
         self.qkv_proj = nn.Linear(embedding_dim, 3 * embedding_dim)
+        self.q = nn.Linear(embedding_dim, embedding_dim)
+        self.k = nn.Linear(embedding_dim, embedding_dim)
+        self.v = nn.Linear(embedding_dim, embedding_dim)
         self.lifting = nn.Linear(embedding_dim, embedding_dim)
         self.masked = masked
         self.softmax = nn.Softmax()
@@ -24,12 +27,12 @@ class MultiheadSelfAttention(nn.Module):
         # ^ essentially, each head gets its own query, key, and value projection (of the same size as the head) of each word in the sequence for each sequence in the batch
         q, k, v = torch.chunk(qkv, 3) # now sets of (N, num_heads, 1 + seq_len, head_size); well, with an extra dimension of 1 at index 0
 
-        a_t = torch.matmul(q.squeeze(), k.squeeze().swapaxes(-2, -1)) * self.scale # (N, num_heads, 1 + seq_len, 1 + seq_len)
+        a_t = torch.matmul(q.squeeze(0), k.squeeze(0).swapaxes(-2, -1)) * self.scale # (N, num_heads, 1 + seq_len, 1 + seq_len)
         a_t = a_t.softmax(dim=-1)
         if self.masked:
             a_t = torch.tril(a_t) # (N, num_heads, 1 + seq_len, 1 + seq_len)
             a_t = a_t / torch.sum(a_t, dim=-1, keepdim=True)
-        attn_vals = torch.matmul(a_t, v.squeeze()).swapaxes(-3, -2).flatten(-2, -1) # concatenates heads; (N, 1 + seq_len, num_heads * head_size) = (N, 1 + seq_len, embedding_dim)
+        attn_vals = torch.matmul(a_t, v.squeeze(0)).swapaxes(-3, -2).flatten(-2, -1) # concatenates heads; (N, 1 + seq_len, num_heads * head_size) = (N, 1 + seq_len, embedding_dim)
         mha_out = self.lifting(attn_vals) # (N, 1 + seq_len, embedding_dim)
 
         return self.dropout(mha_out)
@@ -59,7 +62,7 @@ class TransformerEncoderLayer(nn.Module):
 
 
 class FontModel(nn.Module):
-    def __init__(self, num_layers : int, vocab_size : int, embedding_dim : int, num_heads : int, ff_dim : int):
+    def __init__(self, num_layers : int, vocab_size : int, embedding_dim : int, num_heads : int, ff_dim : int) -> nn.Module:
         super(FontModel, self).__init__()
 
         self.num_layers = num_layers
@@ -68,6 +71,7 @@ class FontModel(nn.Module):
 
         # self.embedder = nn.Linear(vocab_size, embedding_dim)
         self.embedder = nn.Embedding(vocab_size, embedding_dim)
+        self.reverse_embedder = nn.Linear(embedding_dim, vocab_size) # for pre-training
 
         self.transformer_encoder_layers = nn.Sequential(
             *[TransformerEncoderLayer(embedding_dim, num_heads, ff_dim) for _ in range(num_layers)]
@@ -75,7 +79,14 @@ class FontModel(nn.Module):
 
         self.token_space = nn.Linear(embedding_dim, vocab_size)
 
-    def forward(self, x):
+    def identity_embeddings(self, x : torch.Tensor) -> torch.Tensor:
+        '''
+        Used for learning useful embeddings by training an identity function through a bottleneck.
+        The embeddings learned will be used in the regular forward pass after pretraining.
+        '''
+        return self.reverse_embedder(self.embedder(x)).softmax(dim=-1)
+
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
         # x : (batch_size, seq_len, vocab_size)
         embeddings = torch.cat([torch.zeros((x.shape[0], 1, self.embedding_dim)), self.embedder(x)], dim=1)
         encoder_out = self.transformer_encoder_layers(embeddings)
--- runner_runner.py
+++ runner_runner.py
@@ -3,22 +3,28 @@ from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 from fontmodel import FontModel
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("Executing runner_runner.py...\n-----------------------------")
 
-    epochs = 250
-    batch_size = 16
-    lr = 2e-3
-    weight_decay=1e-10
+    pretrain_embeddings = True
+    pretrain_epochs = 20
+
+    print(f"pretraining hyperparameters:\n\t{pretrain_embeddings=}\n\t{pretrain_epochs=}")
+
+    epochs = 150
+    batch_size = 128
+    lr = 4e-3
+    weight_decay=1e-4
 
     print(f"training hyperparameters:\n\t{epochs=}\n\t{batch_size=}\n\t{lr=}\n\t{weight_decay=}")
 
     vocab_size = 12
-    num_layers = 4
-    embedding_dim = 6
-    num_heads = 2
-    ff_dim = 4
+    num_layers = 2
+    embedding_dim = 8
+    num_heads = 4
+    ff_dim = 6
 
     print(f"fontmodel hyperparameters:\n\t{vocab_size=}\n\t{num_layers=}\n\t{embedding_dim=}\n\t{num_heads=}\n\t{ff_dim=}")
 
@@ -34,7 +40,8 @@ if __name__ == "__main__":
     BEGIN TEST SECTION
     '''
     
-    dataset_size = 10000
+    dataset_size = 60000
+    train_dataset_size = (dataset_size * 4) // 5
     elements_per_seq = 5
 
     sample_input = torch.randint(0, vocab_size, (dataset_size, elements_per_seq))
@@ -48,24 +55,60 @@ if __name__ == "__main__":
 
     print("\nCreating dataset....\n")
 
-    tensor_dataset = TensorDataset(sample_input, sample_truths)
-    dataloader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=True)
+    train_tensor_dataset = TensorDataset(sample_input[:train_dataset_size], sample_truths[:train_dataset_size])
+    train_dataloader = DataLoader(train_tensor_dataset, batch_size=batch_size, shuffle=True)
+    test_tensor_dataset = TensorDataset(sample_input[train_dataset_size:], sample_truths[train_dataset_size:])
+    test_dataloader = DataLoader(test_tensor_dataset, batch_size=batch_size, shuffle=True)
+
+    print("\nPretraining embeddings...\n")
+    if pretrain_embeddings:
+        tensor_dataset = TensorDataset(torch.arange(vocab_size).reshape((vocab_size, 1)).repeat((512,1)).long(), torch.arange(vocab_size).reshape((vocab_size, 1)).repeat((512, 1)).long())
+        pretrain_dataloader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=True)
+
+        model.train()
+        loss_fn = torch.nn.BCELoss(reduction='sum')
+        optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+        for epoch in range(pretrain_epochs):
+            total_loss = 0
+            for X, y in tqdm(pretrain_dataloader):
+                optimizer.zero_grad()
+                out = model.identity_embeddings(X)
+                loss = loss_fn(out, torch.nn.functional.one_hot(X, vocab_size).float())
+                total_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+            print(f"Epoch {epoch+1}/{pretrain_epochs} completed. Total Loss = {total_loss/train_dataset_size}")
 
     print("\nTraining model...\n")
 
     model.train()
+    train_loss_list = []
+    test_loss_list = []
     loss_fn = torch.nn.BCELoss(reduction='sum')
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     for epoch in range(epochs):
+        model.train()
         total_loss = 0
-        for X, y in tqdm(dataloader):
+        for X, y in tqdm(train_dataloader):
             optimizer.zero_grad()
             out = model(X)
             loss = loss_fn(out, torch.nn.functional.one_hot(y, vocab_size).float())
             total_loss += loss.item()
             loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch+1}/{epochs} completed. Total Loss = {total_loss/dataset_size}")
+        train_loss_list += [total_loss]
+        
+        model.eval()
+        total_loss = 0
+        for X, y in tqdm(test_dataloader):
+            out = model(X)
+            loss = loss_fn(out, torch.nn.functional.one_hot(y, vocab_size).float())
+            total_loss += loss.item()
+        test_loss_list += [total_loss]
+        print(f"Epoch {epoch+1}/{epochs} completed. Train Loss = {train_loss_list[-1]/train_dataset_size};  Test Loss: {test_loss_list[-1]/(dataset_size - train_dataset_size)}")
+    plt.plot(train_loss_list)
+    plt.plot(test_loss_list)
+    plt.show()
 
     print("\nTesting model...\n")
 
