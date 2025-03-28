854630cfecc7de38fed8393c08aa3c231e76efe5
Nolan Bridges <nolanmbridges@gmail.com>
2025-03-27 21:32:13 -0700

    dockerfile update

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 import torch
 import os
 print(os.getcwd(), __file__, os.path.dirname(__file__), os.listdir(os.path.dirname(__file__)))
-from ml.ldm import LDM
+from ldm import LDM
 
 diff_model = torch.load('models/ldm-basic-33928allchars_centered_scaled_sorted_filtered_(128, 128)-0005-100-1400.pkl').to('cuda', dtype=torch.float32)
 font_model = torch.load('models/transformer-basic-33928allchars_centered_scaled_sorted_filtered_cumulative_padded-14.pkl').to('cuda', dtype=torch.bfloat16)
