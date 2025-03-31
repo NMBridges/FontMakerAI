c9b7c23149d06d991dc08d914a4a691443df8dcc
Nolan Bridges <nolanmbridges@gmail.com>
2025-03-30 19:42:49 -0700

    dockerfile udpate

--- backend/api.py
+++ backend/api.py
@@ -9,6 +9,8 @@ from backend.ml.ldm import LDM
 
 import os
 print(os.getcwd())
+print(os.listdir())
+
 
 diff_model = torch.load('models/ldm-basic-33928allchars_centered_scaled_sorted_filtered_(128,128)-0005-100-1400.pkl').to('cuda', dtype=torch.float32)
 font_model = torch.load('models/transformer-basic-33928allchars_centered_scaled_sorted_filtered_cumulative_padded-14.pkl').to('cuda', dtype=torch.bfloat16)
