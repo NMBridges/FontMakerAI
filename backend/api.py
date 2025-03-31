95c1fda08b2ff5a8b70c32a5262c5f5f2b9d7241
Nolan Bridges <nolanmbridges@gmail.com>
2025-03-30 19:23:13 -0700

    model path update api

--- backend/api.py
+++ backend/api.py
@@ -7,6 +7,9 @@ import numpy as np
 import torch
 from backend.ml.ldm import LDM
 
+import os
+print(os.getcwd())
+
 diff_model = torch.load('models/ldm-basic-33928allchars_centered_scaled_sorted_filtered_(128,128)-0005-100-1400.pkl').to('cuda', dtype=torch.float32)
 font_model = torch.load('models/transformer-basic-33928allchars_centered_scaled_sorted_filtered_cumulative_padded-14.pkl').to('cuda', dtype=torch.bfloat16)
 
