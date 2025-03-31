0d89cbd1ac82e05f393579d38faa0650e93d10ac
Nolan Bridges <nolanmbridges@gmail.com>
2025-03-31 00:49:40 -0700

    debugging

--- backend/api.py
+++ backend/api.py
@@ -13,7 +13,13 @@ sys.path.insert(0, './backend/ml')
 
 diff_model = LDM(diffusion_depth=1024, embedding_dim=2048, num_glyphs=26, label_dim=128, num_layers=24, num_heads=32, cond_dim=128).to('cuda', dtype=torch.float32)
 
-diff_model.load_state_dict(torch.load('./backend/models/ldm-basic-33928allchars_centered_scaled_sorted_filtered-128-128-0005-100-1300.pkl', weights_only=False))
+state_dict = torch.load('./backend/models/ldm-basic-33928allchars_centered_scaled_sorted_filtered-128-128-0005-100-1300.pkl', weights_only=False)
+state_dict['enc_dec.z_min'] = state_dict['z_min']
+state_dict['enc_dec.z_max'] = state_dict['z_max']
+state_dict.pop('z_min')
+state_dict.pop('z_max')
+print(state_dict['ddpm.cond_embedding.weight'].shape)
+diff_model.load_state_dict(state_dict)
 diff_model = diff_model.to('cuda', dtype=torch.float32)
 
 font_model = torch.load('./backend/models/transformer-basic-33928allchars_centered_scaled_sorted_filtered_cumulative_padded-14.pkl', weights_only=False).to('cuda', dtype=torch.bfloat16)
