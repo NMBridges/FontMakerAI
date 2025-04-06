bc6b138d06dd140d3a51c97d1fd7a532a603ff36
Nolan Bridges <nolanmbridges@gmail.com>
2025-04-05 22:41:24 -0700

    bugfix

--- backend/api.py
+++ backend/api.py
@@ -46,7 +46,7 @@ class DiffusionThread(threading.Thread):
         self.progress = 0
         self.label = None
         self.cfg_coeff = 0.0#3.0
-        self.gamma = 1.0
+        self.gamma = 0.0
         self.output = None
         super().__init__()
 
@@ -61,8 +61,8 @@ class DiffusionThread(threading.Thread):
                 t_curr = t
                 t_prev = timesteps[idx-1] if t > 0 else 0
 
-                abar_curr = diff_model.ddpm.alpha_bars[t_curr-1] if t_curr > 0 else torch.ones_like(diff_model.ddpm.alpha_bars[0])
-                abar_prev = diff_model.ddpm.alpha_bars[t_prev-1] if t_prev > 0 else torch.ones_like(diff_model.ddpm.alpha_bars[0])
+                abar_curr = diff_model.ddpm.alpha_bars[t_curr]
+                abar_prev = diff_model.ddpm.alpha_bars[t_prev]
 
                 predicted_noise = diff_model.predict_noise(z, times[t_curr:t_curr+1], self.label)
                 # if self.cfg_coeff > 0:
