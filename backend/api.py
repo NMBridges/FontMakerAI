6fb5f3dd80ccb0536cb887d4bbdcb461da37ef7d
Nolan Bridges <nolanmbridges@gmail.com>
2025-04-06 19:53:36 -0700

    logical not in mask

--- backend/api.py
+++ backend/api.py
@@ -77,7 +77,7 @@ class DiffusionThread(threading.Thread):
                 pred_x0 = (z - predicted_noise * torch.sqrt(1 - abar_curr)) / torch.sqrt(abar_curr)
                 var = (self.eta ** 2) * (1 - abar_curr / abar_prev) * (1 - abar_prev) / (1 - abar_curr)
                 z_prev = torch.sqrt(abar_prev) * pred_x0 + torch.sqrt(1 - abar_prev - var) * predicted_noise + torch.sqrt(var) * torch.randn_like(pred_x0) * (t_prev > 0)
-                z = z_prev * self.masks[:,:,None,None] + latent_input * (1 - self.masks)[:,:,None,None]
+                z = z_prev * self.masks[:,:,None,None] + latent_input * (~self.masks)[:,:,None,None]
 
                 self.progress += 1
             sample_glyphs = diff_model.latent_to_feature(z)
