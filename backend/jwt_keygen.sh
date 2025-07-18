d2e332fcc052353301c01a7c921f8032b57a41dc
Nolan Bridges <nolanmbridges@gmail.com>
2025-07-18 15:09:03 -0700

    JWT keygen debugging

--- backend/jwt_keygen.sh
+++ backend/jwt_keygen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source .env
-ssh-keygen -q -t rsa -N ${JWT_KEY_FONT_API_PASSWORD} -f /id_rsa
+echo "Generating JWT key..."
+echo "Password: ${JWT_KEY_FONT_API_PASSWORD}"
+echo "Command: ssh-keygen -q -t rsa -N ${JWT_KEY_FONT_API_PASSWORD} -f ./id_rsa_font_api"
+echo "Output:"
+ssh-keygen -q -t rsa -N ${JWT_KEY_FONT_API_PASSWORD} -f ./id_rsa_font_api
