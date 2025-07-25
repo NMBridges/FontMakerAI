868b0133c82f4c99d02a6917170cfe2f10635b3a
Nolan Bridges <nolanmbridges@gmail.com>
2025-07-24 18:51:32 -0700

    http -> https for api

--- frontend/99fonts/src/utils.tsx
+++ frontend/99fonts/src/utils.tsx
@@ -1,4 +1,4 @@
 // CORS issue: Remote server needs to allow requests from localhost:5173
 // Solutions: 1) Backend adds CORS headers, 2) Use --disable-web-security Chrome, 3) Use proxy
-export const url_base = 'http://99fonts.com:80';
+export const url_base = 'https://99fonts.com:3000';
 // export const url_base = 'http://localhost:80/';
